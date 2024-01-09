20383c98edbb0a39cf25506ca30a4e97791cea2a
DianaBaghdasaryan1997 <dianabaghdasarian@gmail.com>
2024-01-09 23:54:15 +0400

    ....

---
+++ hardhat-tutorial/test/kk.js
@@ -0,0 +1,15 @@
+//export 'expect' function from javascript's chai assertion library.
+const { expect } = require("chai")
+//export 'loadFixture' function from hardhat's @nomicfoundation/hardhat-toolbox/network-helpers module
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
+// console.log(loadFixture)
+// console.log(expect)
+
+/*Use 'describe' function from javascript's Mocha testing framework to create test suite(to group together related test cases) and 
+'it' keyword to create individual test cases*/
+describe("Token contract", function (){
+
+}
+)
+
+
